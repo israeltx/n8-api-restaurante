0d1661400d1666bed5c86f8544fc9cff4dcf5e27
Israel Teixeira <israeltx@gmail.com>
2025-07-07 11:43:46 -0300

    Create migration for the Products table; Set client back to splite3

--- knexfile.ts
+++ knexfile.ts
@@ -1,7 +1,7 @@
 import { connect } from "http2";
 
 export default {
-  client: 'better-sqlite3',
+  client: 'sqlite3',
   connection: {
     filename: './src/database/database.db'
   },
@@ -9,7 +9,7 @@ export default {
     afterCreate: (connection: any, done: any) => {
       connection.run('PRAGMA foreign_keys = ON')
       done()
-    }
+    },
   },
   useNullAsDefault: true,
   migrations: {
