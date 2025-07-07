9a2f3d60afe273e321380d754fb450dd7d903c5b
Israel Teixeira <israeltx@gmail.com>
2025-07-07 11:34:44 -0300

    Set pool for Knex

--- knexfile.ts
+++ knexfile.ts
@@ -1,7 +1,15 @@
+import { connect } from "http2";
+
 export default {
   client: 'better-sqlite3',
   connection: {
     filename: './src/database/database.db'
+  },
+  pool: {
+    afterCreate: (connection: any, done: any) => {
+      connection.run('PRAGMA foreign_keys = ON')
+      done()
+    }
   },
   useNullAsDefault: true,
   migrations: {
